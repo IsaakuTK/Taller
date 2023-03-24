3adb606a29c5b48c2a444b7c482dd2cd5ea52d1b
unknown <juanestebanrp123mail.com>
2023-03-23 21:03:25 -0500

    6

--- app/components/navbar.ts/index.ts
+++ app/components/navbar.ts/index.ts
@@ -16,16 +16,10 @@ class Bar extends HTMLElement{
                 <link rel="stylesheet" href="./app/components/bar/bar.css">
                 <section class="all">
                 <section class="nagvar1">
-        <img class="logo" src="https://brandemia.org/contenido/subidas/2022/11/nintendo-logo-2022-actual-1024x572.png">
-        <button class="busq"><img class="first" src="https://cdn.pixabay.com/photo/2017/01/13/01/22/magnifying-glass-1976105_960_720.png"><input  type="text" placeholder="Search games, hardware, news, etc."></button>
-        <p>All categories</p>
-        <section class="elpp">
-            <button><img class="first" src="https://cdn-icons-png.flaticon.com/512/1660/1660165.png"> Support </button>
-            <button><img class="first" src="https://cdn-icons-png.flaticon.com/512/4225/4225964.png"> Wish List </button>
-            <button><img class="first" src="https://cdn-icons-png.flaticon.com/512/2838/2838838.png"> Cart </button>
-            <button><img class="first" src="https://cdn-icons-png.flaticon.com/512/2321/2321232.png"> Log in / Sign up </button>
-            </section>
-        </section>
+        <img class="logo" src="./public/app/fotos/Wink.png">
+        <button class="busq"><img class="first" src="https://cdn.pixabay.com/photo/2017/01/13/01/22/magnifying-glass-1976105_960_720.png"><input  type="text" placeholder="Search in Wink."></button>
+        <img class="upload" src"https://img.icons8.com/ios-glyphs/256/send-letter.png">
+        <img class="perfil" src"http://www.shenyunperformingarts.org/data/image/original/2021/05/29/63ea2c642aaee001d818604fe1d9a811.jpg">
                 `;
             }
         }
